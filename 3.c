b39760b099ad28befb61d33fdd840a0eef8bec18
ksb7 <70291457+ksb7@users.noreply.github.com>
2021-06-03 21:38:45 +0300

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define true 1
+
+int main ()
+{
+    int n;
+    do
+    {
+        printf("numarul populatiei de inceput\n");
+        scanf("%d", &n);
+        if (n>9)  {printf ("\n");break;} else {printf ("numarul populatiei nu poate fi mai mic de 9\n");
+        continue;}
+    } while (true);
+    int m;
+    do
+    {
+        printf("numarul populatiei finale\n");
+        scanf("%d", &m);
+        if (m>9)  break; else {printf ("numarul populatiei nu poate fi mai mic sau egal cu 9\n");
+        continue;}
+    } while (true);
+    int k=0;
+    while(n<=m)
+    {
+        n=n+(n/3)-(n/4);
+        k++;
+    }
+    printf ("%d ani ii va lua sa creasca\n", k);
+    return 0;
+}
